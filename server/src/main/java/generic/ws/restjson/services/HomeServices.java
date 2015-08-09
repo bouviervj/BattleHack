13377c99643f5606fbf1cd7590c5241b652f4912
JBV <bouviervj@gmail.com>
2015-08-09 01:38:42 -0400

    Added business classes

---
+++ server/src/main/java/generic/ws/restjson/services/HomeServices.java
@@ -0,0 +1,37 @@
+package generic.ws.restjson.services;
+
+import java.util.HashMap;
+import java.util.Vector;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.SecurityContext;
+
+import org.apache.log4j.Logger;
+
+import server.Protocol;
+import server.transaction.Reply;
+import server.transaction.Device;
+
+@Path("/v1/home")
+public class HomeServices {
+
+	protected static final Logger LOGGER = Logger.getLogger(HomeServices.class.getName());
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/devices")
+	public  HashMap<String,Vector<Device>> devicess( @Context SecurityContext sc ) {
+		LOGGER.info("Retrieve devices ...");
+		return Protocol._deviceMap;
+	}
+			
+	
+}
---
+++ server/src/main/java/server/business/Home.java
@@ -0,0 +1,13 @@
+package server.business;
+
+import java.util.Vector;
+
+import server.transaction.Device;
+
+public class Home {
+
+	public String owner;
+	public Vector<Device> devices;
+	
+
+}
